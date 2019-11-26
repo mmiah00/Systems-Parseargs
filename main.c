38bd353806d92e92ff863fef3ff6bec0fb16d27c
mmiah00 <mmiah00@stuy.edu>
2019-11-26 10:46:19 -0500

    wrote function not tested

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> 
+#include <unistd.h>
 
 char ** parse_args (char * line);
+
+char ** parse_args (char * line) {
+  char ** ans;
+  int i = 0;
+  char * now;
+  while (now != NULL) {
+    ans [i] = strcep (now, " ");
+    i ++;
+  }
+  return ans; 
+}
+int main () {
+
+  return 0;
+}
